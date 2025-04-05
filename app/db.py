fbed5d0b9bd9eebe6f7ea7943f8ac6463b2909d5
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2025-04-05 12:08:51 +0400

    fix docker compose

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ import os
 import sqlite3
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "..", "db.sqlite3")
+DB_PATH = "/app/db.sqlite3"
 
 conn = sqlite3.connect(DB_PATH, check_same_thread=False)
 cursor = conn.cursor()
